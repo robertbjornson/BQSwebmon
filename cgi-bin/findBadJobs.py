06ac8a182c7c9ba1c050a7c5435fcd58d75290fb
Robert Bjornson <rbjornson@gmail.com>
2017-09-29 15:54:20 -0400

    limited output to over thresh

--- cgi-bin/findBadJobs.py
+++ cgi-bin/findBadJobs.py
@@ -1,7 +1,7 @@
 import slurmBQS
 
 
-def getSuspects(limit):
+def getSuspects(limit, corelimit):
     info = slurmBQS.getInfo()
 
     J=info['jobs']
@@ -15,24 +15,25 @@ def getSuspects(limit):
                 n=N[nid]
                 l+=float(cnt)/n['ncpus']*n['loadave']
             r=l/j['cores']
-            if r < 0.25:
+            if r < 0.25 and j['cores']>=corelimit:
                 suspects.append((j['cores'],r, jid, j))
     return sorted(suspects, reverse=True)
 
-def findUsers(s):
+def findUsers(s, lim):
     users={}
     for c, r, jid, j in s:
         users[j['owner']]=users.setdefault(j['owner'], 0)+c
-    l=sorted([(c, u) for u, c in users.items()], reverse=True)
+    l=sorted([(c, u) for u, c in users.items() if c > lim], reverse=True)
     return l
 
 if __name__=='__main__':
-    s=getSuspects(0.3)
+    s=getSuspects(0.3, 20)
+    print "%s\t%s\t\t%s\t%20s\t%20s\t%s" %("cores", "eff", "jid", "owner", "name", "hosts")
     for c, r, jid, j in s:
         print "%d\t%f\t%s\t%20s\t%20s\t%s" %(c, r, jid, j['owner'], j['name'], j['hosts'])
-    for c, u in findUsers(s):
+    print "\n\ncores\tuser"
+    for c, u in findUsers(s,20):
         print "%d\t%s" % (c, u)
-#        print "%20s%d" % (u, c)
 
 
     
